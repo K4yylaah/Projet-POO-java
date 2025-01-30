5dbfc6fca87dde66d3f8a1007bd054b1ecc1f853
Idris OUJDID <idris.oujdid@edu.esiee-it.fr>
2025-01-30 09:36:35 +0100

    début de la fonction pour ajouter un produit à la liste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char a = input.next().charAt(0);
+        System.out.println("quel type de produit sont à ajouté ? " + a);
 
     }
 }
